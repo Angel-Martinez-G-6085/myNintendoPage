456a0bf55af436431a0a6cccfbeccebb0f993768
Matinez Garrido Angel Ivan <ben6085@gmail.com>
2021-03-26 18:08:05 -0600

    funcion del boton derecho del slider terminada

---
+++ js/index.js
@@ -0,0 +1,22 @@
+let advertaisments = ['../assets/images/advertaisments/anuncio-1.jpg',
+    '../assets/images/advertaisments/anuncio-2.jpg',
+    '../assets/images/advertaisments/anuncio-3.jpg'],
+    counter = 0,
+    rightArrow = document.querySelector('.slider--container-RightArrow'),
+    leftArrow = document.querySelector('.slider--container-LeftArrow');
+
+function rightArrowButton(rArrow) {
+    let img = document.querySelector('.slider--image');
+    rArrow.addEventListener('click', () => {
+        if (counter <= advertaisments.length-1) {
+            counter++;
+            img.src = advertaisments[counter];
+        }
+        if(counter==advertaisments.length){
+            counter=0;
+            img.src=advertaisments[counter];
+        }
+    });
+}
+
+rightArrowButton(rightArrow);
